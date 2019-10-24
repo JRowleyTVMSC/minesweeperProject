bb95abf18e9bd27ba4eeb279c0680c442cab186f
Jamison <jamisonrowley@gmail.com>
2019-10-24 02:04:55 -0600

    added brief explanation of error for clarity

--- Minesweeper2.py
+++ Minesweeper2.py
@@ -44,13 +44,13 @@ clock = pygame.image.load("time.png")
 boxes = []
 numBombs = 0
 
-class box():
-	#flag = -3 # -3 = NO FLAG; -2 = FLAGGED; -1 = ?; 0 = BLANK; 1-8 = WARN; 9 = Exploded
+class box() :
+	# flag = -3 # -3 = NO FLAG; -2 = FLAGGED; -1 = ?; 0 = BLANK; 1-8 = WARN; 9 = Exploded
 	isBomb = False
 	index = 0
 	x = 0
 	y = 0
-	def __init__(self, boxPos):
+	def __init__(self, boxPos) :
 		self.boxP = boxPos
 		self.flag = -3
 
@@ -77,11 +77,11 @@ def drawBoxesInit():
 
 drawBoxesInit()
 
-def pickBombs():
+def pickBombs() :
 	global numBombs
-	while numBombs < 10:
+	while numBombs < 10 :
 		x = randrange(0, len(boxes))
-		if not boxes[x].isBomb:
+		if not boxes[x].isBomb :
 			boxes[x].isBomb = True
 			numBombs += 1
 
@@ -90,124 +90,123 @@ flagsUsed = 0
 checkedBoxes = []
 blocksLeft = 0
 
-def drawBoxes():
+def drawBoxes() :
 	global flagsUsed
 	global blocksLeft
 	flagsUsed = 0
 	blocksLeft = 0
-	for b in boxes:
-		if b.flag == -3:
+	for b in boxes :
+		if b.flag == -3 :
 			blocksLeft += 1
 			surfObj.blit(blockSurf, b.boxP)
-		elif b.flag == 0:
-			if not b in checkedBoxes:
+		elif b.flag == 0 :
+			if not b in checkedBoxes :
 				pathFind(b)
 			surfObj.blit(blockSurfBlank, b.boxP)
-		elif b.flag == 9:
+		elif b.flag == 9 :
 			surfObj.blit(explode, b.boxP)
 			lose()
-		elif b.flag == -2:
+		elif b.flag == -2 :
 			surfObj.blit(flag, b.boxP)
 			flagsUsed += 1
-		elif b.flag == -1: surfObj.blit(question, b.boxP)
-		elif b.flag == 1: surfObj.blit(warn1, b.boxP)
-		elif b.flag == 2: surfObj.blit(warn2, b.boxP)
-		elif b.flag == 3: surfObj.blit(warn3, b.boxP)
-		elif b.flag == 4: surfObj.blit(warn4, b.boxP)
-		elif b.flag == 5: surfObj.blit(warn5, b.boxP)
-		elif b.flag == 6: surfObj.blit(warn6, b.boxP)
-		elif b.flag == 7: surfObj.blit(warn7, b.boxP)
-		elif b.flag == 8: surfObj.blit(warn8, b.boxP)
-	if blocksLeft == 10: win()
+		elif b.flag == -1 : surfObj.blit(question, b.boxP)
+		elif b.flag == 1 : surfObj.blit(warn1, b.boxP)
+		elif b.flag == 2 : surfObj.blit(warn2, b.boxP)
+		elif b.flag == 3 : surfObj.blit(warn3, b.boxP)
+		elif b.flag == 4 : surfObj.blit(warn4, b.boxP)
+		elif b.flag == 5 : surfObj.blit(warn5, b.boxP)
+		elif b.flag == 6 : surfObj.blit(warn6, b.boxP)
+		elif b.flag == 7 : surfObj.blit(warn7, b.boxP)
+		elif b.flag == 8 : surfObj.blit(warn8, b.boxP)
+	if blocksLeft == 10 : win()
 
 
-def isInBounds():
+def isInBounds() :
 	return Rect(10, 10, 153, 153).collidepoint(e.pos)
 
-def overBlock():
-	for b in boxes:
-		if b.boxP.collidepoint(e.pos):
+def overBlock() :
+	for b in boxes :
+		if b.boxP.collidepoint(e.pos) :
 			return b
 
-def boxAt(x, y):
-	for b in boxes:
-		if b.x == x and b.y == y:
+def boxAt(x, y) :
+	for b in boxes :
+		if b.x == x and b.y == y :
 			return b
 
 def warnInteger(b):
 	try:
-		if b.x < 9 and b.x > 0 and b.y > 0 and b.y < 9:
+		if b.x < 9 and b.x > 0 and b.y > 0 and b.y < 9 :
 			return (boxAt(b.x - 1, b.y), boxAt(b.x + 1, b.y), boxAt(b.x, b.y - 1), boxAt(b.x, b.y + 1), boxAt(b.x - 1, b.y - 1), boxAt(b.x + 1, b.y +1), boxAt(b.x + 1, b.y - 1), boxAt(b.x - 1, b.y + 1))
-		elif b.x > 8 and b.y > 0 and b.y < 9:
+		elif b.x > 8 and b.y > 0 and b.y < 9 :
 			return (boxAt(b.x - 1, b.y), boxAt(b.x - 1, b.y - 1), boxAt(b.x - 1, b.y + 1))
-		elif b.x < 1 and b.y > 0 and b.y < 9:
+		elif b.x < 1 and b.y > 0 and b.y < 9 :
 			return (boxAt(b.x + 1, b.y), boxAt(b.x + 1, b.y - 1), boxAt(b.x + 1, b.y + 1), boxAt(b.x, b.y+1), boxAt(b.x, b.y-1))
-		elif b.y < 1 and b.x > 0 and b.x < 9:
+		elif b.y < 1 and b.x > 0 and b.x < 9 :
 			return (boxAt(b.x, b.y + 1), boxAt(b.x + 1, b.y + 1), boxAt(b.x-1, b.y), boxAt(b.x+1, b.y), boxAt(b.x-1, b.y+1))
-		elif b.y == 0 and b.x == 0:
+		elif b.y == 0 and b.x == 0 :
 			return (boxAt(1, 0), boxAt(1, 1), boxAt(0, 1))
 	except Exception as msg: print("")
 
-def getWarn(b):
+def getWarn(b) :
 	warn = 0
-	try:
-		for b in warnInteger(b):
-			try:
+	try :
+		for b in warnInteger(b) :
+			try :
 				if b.isBomb: warn += 1
-			except: print("")
+			except : print("")
 		return warn
-	except: print("")
+	except : print("")
 
 boxesToPath = []
 
-def crossFind(b):
-	if b.x == 0 and b.y == 0:
+def crossFind(b) :
+	if b.x == 0 and b.y == 0 :
 		return (boxAt(0, 1), boxAt(1, 0))
-	elif b.x == 0 and b.y == 9:
+	elif b.x == 0 and b.y == 9 :
 		return (boxAt(0, 8), boxAt(1, 9))
-	elif b.x == 9 and b.y == 0:
+	elif b.x == 9 and b.y == 0 :
 		return (boxAt(8, 0), boxAt(9, 1))
-	elif b.x == 9 and b.y == 9:
+	elif b.x == 9 and b.y == 9 :
 		return (boxAt(8, 9), boxAt(9, 8))
-	elif b.x > 0  and b.x < 9 and b.y > 0:
+	elif b.x > 0  and b.x < 9 and b.y > 0 :
 		return (boxAt(b.x, b.y-1), boxAt(b.x-1, b.y), boxAt(b.x+1, b.y), boxAt(b.x, b.y+1))
-	elif b.x == 0 and b.y > 0 and b.y < 9:
+	elif b.x == 0 and b.y > 0 and b.y < 9 :
 		return (boxAt(b.x, b.y-1), boxAt(b.x, b.y+1), boxAt(b.x+1, b.y))
-	elif b.x == 9 and b.y > 0 and b.y < 9:
+	elif b.x == 9 and b.y > 0 and b.y < 9 :
 		return (boxAt(b.x-1, b.y), boxAt(b.x, b.y-1), boxAt(b.x, b.y+1))
-	elif b.y == 0 and b.x > 0 and b.x < 9:
+	elif b.y == 0 and b.x > 0 and b.x < 9 :
 		return (boxAt(b.x - 1, b.y), boxAt(b.x+1, b.y), boxAt(b.x, b.y+1))
-	elif b.y == 9 and b.x > 0 and b.x < 9:
+	elif b.y == 9 and b.x > 0 and b.x < 9 :
 		return (boxAt(b.x - 1, b.y), boxAt(b.x+1, b.y), boxAt(b.x, b.y-1))
 
-def pathFind(b):
-	for bo in crossFind(b):
-		if getWarn(bo) == 0:
+def pathFind(b) :
+	for bo in crossFind(b) :
+		if getWarn(bo) == 0 :
 			bo.flag = 0
-	for bo2 in warnInteger(b):
+	for bo2 in warnInteger(b) :
 		w = getWarn(bo2)
-		if w > 0 and w < 9:
+		if w > 0 and w < 9 :
 			bo2.flag = w
 	checkedBoxes.append(b)
 
-lastGame = 0
+# Only standing error is at 189, data type error where 'w' is being set as 'none' somewhere above.
+# Tried tracing it, but either I'm too tired or too braindead to see what I did wrong
+# Traced from getWarn to warnInteger. warnint is a lot of spaghetti though. Likely math related error
 
-def lose():
+# Doesnt always trip error, either. Seems to be a thing that happens when my white space reveal functionality tries to reveal a bomb
+
+
+def lose() :
 	easygui.msgbox("You blew up!", "You lost!")
 	resetGame()
 
-def win():
+def win() :
 	easygui.msgbox("You win!", "Congrats!")
 	resetGame()
 
-def resetGame():
-	global fps
-	global boxes
-	global numBombs
-	global flagsUsed
-	global checkedBoxes
-	global blocksLeft
-	global lastGame
+def resetGame() :
+	global fps, boxes, numBombs, flagsUsed, checkedBoxes, blocksLeft
 	fps = pygame.time.Clock()
 	boxes[:] = []
 	numBombs = 0
@@ -216,39 +215,36 @@ def resetGame():
 	flagsUsed = 0
 	checkedBoxes = []
 	blocksLeft = 0
-	lastGame = pygame.time.get_ticks()
 
-
-while True:
+while True :
 	surfObj.fill(white)
 	surfObj.blit(bomb, (147, 173))
-
 
 	drawBox()
 	drawBoxes()
 
-	for e in pygame.event.get():
-		if e.type == QUIT:
+	for e in pygame.event.get() :
+		if e.type == QUIT :
 			pygame.quit()
 			sys.exit()
-		elif e.type == MOUSEMOTION:
+		elif e.type == MOUSEMOTION :
 			selBlock = overBlock()
-			if not isInBounds():
+			if not isInBounds() :
 				selBlock = refBlock
-		elif e.type == MOUSEBUTTONUP:
-			if isInBounds():
-				if e.button == 1:
+		elif e.type == MOUSEBUTTONUP :
+			if isInBounds() :
+				if e.button == 1 :
 					selBlock.flag = getWarn(selBlock)
-					if selBlock.flag == 0:
+					if selBlock.flag == 0 :
 						boxesToPath.append(selBlock)
 						pathFind(selBlock)
 					if selBlock.isBomb: selBlock.flag = 9
-				elif e.button == 3:
-					if selBlock.flag == -3:
+				elif e.button == 3 :
+					if selBlock.flag == -3 :
 						selBlock.flag = -2
-					elif selBlock.flag == -2:
+					elif selBlock.flag == -2 :
 						selBlock.flag = -1
-					elif selBlock.flag == -1:
+					elif selBlock.flag == -1 :
 						selBlock.flag = -3
 	if selBlock.flag == -3: surfObj.blit(blockSurfSel, selBlock.boxP)
 
